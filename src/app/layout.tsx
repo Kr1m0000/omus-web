cf18f4e0faa072c95218608fcda7cb32327935de
Kr1m0000 <130426519+Kr1m0000@users.noreply.github.com>
2025-04-25 21:27:43 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,32 +8,32 @@ import { Footer } from "@/sections/Footer";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "K-Services",
-  description: "Digital solutions services for web and mobile Apps",
+  title: "O'Mus",
+  description: "Votre plaisir gustatif",
   icons: {
     icon: "/favicon.png",
   },
-  openGraph: {
-    title: "K-Services",
-    description: "Digital solutions for web and mobile apps.",
-    url: "https://krimou.vercel.app", // Replace with your actual URL
-    siteName: "K-Services",
-    images: [
-      {
-        url: "/product-image.png", // Path to your thumbnail image
-        width: 1200,
-        height: 630,
-        alt: "K-Services preview image",
-      },
-    ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "K-Services",
-    description: "Digital solutions for web and mobile apps.",
-    images: ["/product-image.png"],
-  },
+  // openGraph: {
+  //   title: "K-Services",
+  //   description: "Digital solutions for web and mobile apps.",
+  //   url: "https://krimou.vercel.app", // Replace with your actual URL
+  //   siteName: "K-Services",
+  //   images: [
+  //     {
+  //       url: "/product-image.png", // Path to your thumbnail image
+  //       width: 1200,
+  //       height: 630,
+  //       alt: "K-Services preview image",
+  //     },
+  //   ],
+  //   type: "website",
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "K-Services",
+  //   description: "Digital solutions for web and mobile apps.",
+  //   images: ["/product-image.png"],
+  // },
 };
 
 export default function RootLayout({
